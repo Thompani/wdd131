d9d56827366926002c41d6fc3ec0ce694ad1d477
Aniekan Thompson <greatestthompson@gmail.com>
2024-12-13 23:51:11 +0100

    final edit

--- filtered_temples.js
+++ filtered_temples.js
@@ -184,3 +184,12 @@ function creatTempleCard(filteredTemples){
 	document.querySelector(".container").appendChild(card);
 });
 }
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
